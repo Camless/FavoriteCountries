87c57f34b59913e861c527602fb24dc3aa6de0b5
Carlos <morales22@outlook.com>
2022-04-01 15:18:17 -0500

    Initial Commit

---
+++ FavoriteCountries/FavoriteCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCountriesApp.swift
+//  FavoriteCountries
+//
+//  Created by Carlos Morales III on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
